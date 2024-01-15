f605e07cfccb351b3385963b5cf5157e9738c208
mikha-dev <mikha.dev@gmail.com>
2024-01-15 19:29:33 +0300

    prop

--- src/Http/Controllers/Dashboards/PropController.php
+++ src/Http/Controllers/Dashboards/PropController.php
@@ -2,11 +2,17 @@
 
 namespace D4T\Admin\Demos\Http\Controllers\Dashboards;
 
+use Dcat\Admin\DcatIcon;
 use Dcat\Admin\Layout\Row;
+use Dcat\Admin\Widgets\Steps;
+//use Illuminate\Routing\Controller;
 use Dcat\Admin\Layout\Content;
 use Dcat\Admin\Traits\PreviewCode;
-//use Illuminate\Routing\Controller;
+use Dcat\Admin\Widgets\IconicLink;
 use Illuminate\Routing\Controller;
+use Dcat\Admin\Widgets\FeaturedCard;
+use Dcat\Admin\Widgets\BadgeWithIcon;
+use Dcat\Admin\Widgets\IconWithToolTip;
 use D4T\Admin\Demos\Http\Widgets\AccountDataCard;
 use D4T\Admin\Demos\Http\Widgets\TradeHistoryCard;
 use D4T\Admin\Demos\Http\Widgets\BalanceChartWidget;
@@ -22,6 +28,29 @@ class PropController extends Controller
         return $content
             ->header('Prop Dashboard')
             ->description('Account Statistics...')
+            ->body(function (Row $row) {
+
+                $widget = new Steps();
+
+                $widget->add('your are here', 'Step1 - Challenge Account', DcatIcon::HOME(true), true);
+                $widget->add('Next Phase', 'Step2 - Funded Account', DcatIcon::HOME(true));
+
+                $row->column(12, $widget);
+            })
+            ->body(function (Row $row) {
+
+                $features = new FeaturedCard();
+                $features->icon(DcatIcon::HOME(true));
+
+                $features->addFeature(new BadgeWithIcon(__('userdash.plan_type'), 'description', new IconWithToolTip(DcatIcon::HELP(), __('userdash.help_plan_type'))));
+                $features->addFeature(new BadgeWithIcon(__('userdash.platform'), 'title', new IconWithToolTip(DcatIcon::HELP(), __('userdash.help_platform'))));
+                $features->addFeature(new BadgeWithIcon(__('userdash.starting_balance'), 'title', new IconWithToolTip(DcatIcon::HELP(), __('userdash.help_starting_balance'))));
+
+                $features->footerAddElement(new IconicLink(DcatIcon::EMAIL(true), __('userdash.contact_support'), config('admin.contact-us-link', '')));
+                $features->footerAddElement(new IconicLink(DcatIcon::SHARE_ALT(true), __('userdash.share_statistics'), config('admin.contact-us-link', '')));
+
+                $row->column(12, $features);
+            })
             ->body(function (Row $row) {
 
                 $widgetBalance = new BalanceChartWidget();
@@ -44,6 +73,7 @@ class PropController extends Controller
 
                 $history = new TradeHistoryCard();
                 $row->column(12, $history);
-            });
+            })
+            ;
     }
 }
