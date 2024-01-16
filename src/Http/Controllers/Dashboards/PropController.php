08b3b9171dc5281b9fbdee94d24e877b8e5e0541
dtikhonov <dtihonov@code-pilots.ru>
2024-01-16 15:42:06 +0300

    fix after merge

--- src/Http/Controllers/Dashboards/PropController.php
+++ src/Http/Controllers/Dashboards/PropController.php
@@ -2,10 +2,10 @@
 
 namespace D4T\Admin\Demos\Http\Controllers\Dashboards;
 
+use Dcat\Admin\DcatIcon;
 use Dcat\Admin\Layout\Row;
 use Dcat\Admin\Layout\Content;
 use Dcat\Admin\Traits\PreviewCode;
-//use Illuminate\Routing\Controller;
 use Illuminate\Routing\Controller;
 use D4T\Admin\Demos\Http\Widgets\AccountDataCard;
 use D4T\Admin\Demos\Http\Widgets\TradeHistoryCard;
@@ -13,8 +13,6 @@ use D4T\Admin\Demos\Http\Widgets\BalanceChartWidget;
 use D4T\Admin\Demos\Http\Widgets\TradingObjectiveCard;
 use D4T\Admin\Demos\Http\Widgets\ProfitTargetChartCard;
 use D4T\Core\Enums\StyleClassType;
-use Dcat\Admin\Widgets\BadgeDot;
-use Dcat\Admin\Widgets\SimpleCard;
 use Dcat\Admin\Widgets\ApexCharts\RadialBarChart;
 
 class PropController extends Controller
@@ -29,8 +27,9 @@ class PropController extends Controller
             ->body(function (Row $row) {
 
                 $widgetBalance = new BalanceChartWidget();
-                $row->column(9, $widgetBalance);
-
+                $row->column(12, $widgetBalance);
+            })
+            ->body(function (Row $row) {
                 $radialBar = new RadialBarChart();
                 $radialBar->value((3000/5000)*100);
                 $radialBar->hollowSize(30);
@@ -51,7 +50,6 @@ class PropController extends Controller
                     $radialBar
                 ))
                     ->icon(DcatIcon::CALENDAR());
-
                 $row->column(3, $widgetProfitTarget);
             })
             ->body($this->newline())
