931ab4c7fd265b92d3ddf16efdd4e8e3acaf0a7e
mikha-dev <mikha.dev@gmail.com>
2024-01-16 11:30:03 +0300

    fixed table

--- src/Http/Controllers/TableController.php
+++ src/Http/Controllers/TableController.php
@@ -6,11 +6,11 @@ namespace D4T\Admin\Demos\Http\Controllers;
 use Faker\Factory;
 use Dcat\Admin\Layout\Row;
 use Dcat\Admin\Widgets\Card;
-use Dcat\Admin\Widgets\Table;
 use Dcat\Admin\Layout\Content;
+use Dcat\Admin\Widgets\TableAdv;
+use D4T\Core\Enums\StyleClassType;
 use Dcat\Admin\Traits\PreviewCode;
 use Illuminate\Routing\Controller;
-use D4T\Core\Enums\StyleClassType;
 
 class TableController extends Controller
 {
@@ -36,54 +36,54 @@ class TableController extends Controller
         }
 
         $content->row(function(Row $row) use($headers, $rows) {
-            $table1 = new Table($headers, $rows);
+            $table1 = new TableAdv($headers, $rows);
 
             $row->column(12, new Card('Basic', $table1));
         });
 
         $content->row(function(Row $row) use($headers, $rows) {
-            $table1 = new Table($headers, $rows, StyleClassType::DARK);
+            $table1 = new TableAdv($headers, $rows, StyleClassType::DARK);
 
             $row->column(12, new Card('Colored', $table1));
         });
 
         $content->row(function(Row $row) use($headers, $rows) {
-            $table1 = new Table($headers, $rows);
+            $table1 = new TableAdv($headers, $rows);
             $table1->headerClass(StyleClassType::DANGER);
 
             $row->column(12, new Card('Colored Header', $table1));
         });
 
         $content->row(function(Row $row) use($headers, $rows) {
-            $table1 = new Table($headers, $rows);
+            $table1 = new TableAdv($headers, $rows);
             $table1->withFooter();
 
             $row->column(12, new Card('With Footer', $table1));
         });
 
         $content->row(function(Row $row) use($headers, $rows) {
-            $table1 = new Table($headers, $rows);
+            $table1 = new TableAdv($headers, $rows);
             $table1->striped();
 
             $row->column(12, new Card('Striped', $table1));
         });
 
         $content->row(function(Row $row) use($headers, $rows) {
-            $table1 = new Table($headers, $rows);
+            $table1 = new TableAdv($headers, $rows);
             $table1->withBorder();
 
             $row->column(12, new Card('Bordered', $table1));
         });
 
         $content->row(function(Row $row) use($headers, $rows) {
-            $table1 = new Table($headers, $rows);
+            $table1 = new TableAdv($headers, $rows);
             $table1->withHover();
 
             $row->column(12, new Card('With Hover', $table1));
         });
 
         $content->row(function(Row $row) use($headers, $rows) {
-            $table1 = new Table($headers, $rows);
+            $table1 = new TableAdv($headers, $rows);
             $table1->small();
 
             $row->column(12, new Card('Small', $table1));
