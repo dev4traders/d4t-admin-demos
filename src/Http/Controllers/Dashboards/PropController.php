80ae1b3a84b385b894c62bfb5b90525829c7bffb
dtikhonov <dtihonov@code-pilots.ru>
2024-01-17 20:39:01 +0300

    D4T-8: запилил виджет фич

--- src/Http/Controllers/Dashboards/PropController.php
+++ src/Http/Controllers/Dashboards/PropController.php
@@ -4,17 +4,16 @@ namespace D4T\Admin\Demos\Http\Controllers\Dashboards;
 
 use Dcat\Admin\DcatIcon;
 use Dcat\Admin\Layout\Row;
+use Dcat\Admin\Widgets\DropdownItem;
+use Dcat\Admin\Widgets\DropdownWithIcon;
 use Dcat\Admin\Widgets\Steps;
 use Dcat\Admin\Layout\Content;
-use Dcat\Admin\Widgets\BadgeDot;
 use D4T\Core\Enums\StyleClassType;
 use Dcat\Admin\Traits\PreviewCode;
 use Dcat\Admin\Widgets\IconicLink;
-use Dcat\Admin\Widgets\SimpleCard;
 use Illuminate\Routing\Controller;
 use Dcat\Admin\Widgets\FeaturedCard;
 use Dcat\Admin\Widgets\BadgeWithIcon;
-use Dcat\Admin\Widgets\IconWithToolTip;
 use D4T\Admin\Demos\Http\Widgets\StatisticsCard;
 use D4T\Admin\Demos\Http\Widgets\AccountDataCard;
 use D4T\Admin\Demos\Http\Widgets\TradeHistoryCard;
@@ -34,16 +33,51 @@ class PropController extends Controller
             ->header('Prop Dashboard')
             ->description('Account Statistics...')
             ->body(function (Row $row) {
+                $features = new FeaturedCard('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id es');
+                $features->icon('IMG NOT LOADED');
+                $features->headerContent(
+                    (new DropdownWithIcon([
+                        new DropdownItem('Menu item 1'),
+                        new DropdownItem('Menu item 2'),
+                        new DropdownItem('Menu item 3'),
+                    ]))
+                        ->title('$50.000 Challenge')
+                        ->description('Some description')
+                );
 
-                $features = new FeaturedCard('Intermediate Instigator');
-                $features->icon('LIVE');
+                $features->addFeature(
+                    new BadgeWithIcon(
+                        DcatIcon::HELP(),
+                        StyleClassType::PRIMARY,
+                        'Multiple Asset Classes'
+                    )
+                );
+                $features->addFeature(
+                    new BadgeWithIcon(
+                        DcatIcon::HELP(),
+                        StyleClassType::PRIMARY,
+                        'No limits on trading style!'
+                    )
+                );
+                $features->addFeature(
+                    new BadgeWithIcon(
+                        DcatIcon::HELP(),
+                        StyleClassType::PRIMARY,
+                        'The best trading conditions'
+                    )
+                );
+                $features->addFeature(
+                    new BadgeWithIcon(
+                        DcatIcon::HELP(),
+                        StyleClassType::PRIMARY,
+                        'Receive up to 90% of the profit'
+                    )
+                );
 
-                $features->addFeature(new BadgeWithIcon(DcatIcon::HELP(true), StyleClassType::PRIMARY, 'Type' ) );
+                $features->addFooterLink(new IconicLink(DcatIcon::EMAIL(true), 'Contact Support', config('admin.contact-us-link', '')));
+                $features->addFooterLink(new IconicLink(DcatIcon::SHARE_ALT(true), 'Share Statistics', config('admin.contact-us-link', '')));
 
-                // $features->addFooterElement(new IconicLink(DcatIcon::EMAIL(true), 'Contact Support', config('admin.contact-us-link', '')));
-                // $features->addFooterElement(new IconicLink(DcatIcon::SHARE_ALT(true), 'Share Statistics', config('admin.contact-us-link', '')));
-
-                $row->column(12, new SimpleCard(null, $features));
+                $row->column(12, $features);
             })
             ->body(function (Row $row) {
 
