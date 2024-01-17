8c86fc51c08d2c34172b2be29e21e292dfda617b
mikha-dev <mikha.dev@gmail.com>
2024-01-17 13:36:49 +0300

    dailyloss fixes

--- src/Http/Widgets/DailyLossMetricsCard.php
+++ src/Http/Widgets/DailyLossMetricsCard.php
@@ -2,28 +2,28 @@
 declare(strict_types=1);
 namespace D4T\Admin\Demos\Http\Widgets;
 
-use D4T\Core\Enums\StyleClassType;
 use Dcat\Admin\DcatIcon;
-use Dcat\Admin\Widgets\ApexCharts\DonutChart;
 use Dcat\Admin\Widgets\BadgeDot;
+use D4T\Core\Enums\StyleClassType;
 use Dcat\Admin\Widgets\BadgeWithIcon;
 use Dcat\Admin\Widgets\Cards\MetricsCard;
+use Dcat\Admin\Widgets\ApexCharts\RadialBarChart;
 
 class DailyLossMetricsCard extends MetricsCard
 {
     public function __construct()
     {
-        $donutChart = new DonutChart();
-        $donutChart->value([33,33,33]);
-        $donutChart->hollowSize(60);
-        $donutChart->height(150);
-        $donutChart->startAngle(-90);
-        $donutChart->endAngle(90);
-        $donutChart->padding([
+        $bar = new RadialBarChart();
+        $bar->value(89);
+        $bar->hollowSize(60);
+        $bar->height(150);
+        $bar->startAngle(-90);
+        $bar->endAngle(90);
+        $bar->padding([
             'top' => -10,
         ]);
-        $donutChart->fontSize(16);
-        $donutChart->valueOffsetY(-2);
+        $bar->fontSize(16);
+        $bar->valueOffsetY(-2);
 
         $title = 'Daily loss';
         $badgeText = 'Failed';
@@ -31,7 +31,7 @@ class DailyLossMetricsCard extends MetricsCard
         $valueText = '-$451';
         $targetText = 'Target 10';
 
-        parent::__construct($title, $donutChart);
+        parent::__construct($title, $bar);
         $this->tool(new BadgeDot($badgeClass, $badgeText));
         $this->icon(new BadgeWithIcon(DcatIcon::ARROW_TREND_UP(), StyleClassType::SECONDARY));
         $this->value = $valueText;
