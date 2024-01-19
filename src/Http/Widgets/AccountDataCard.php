f503893377a6b28b7d6fd509b875eec95fdd08a4
mikha-dev <mikha.dev@gmail.com>
2024-01-19 11:19:51 +0300

    link

--- src/Http/Widgets/AccountDataCard.php
+++ src/Http/Widgets/AccountDataCard.php
@@ -1,25 +1,36 @@
 <?php
+
 declare(strict_types=1);
+
 namespace D4T\Admin\Demos\Http\Widgets;
 
 use Dcat\Admin\DcatIcon;
 use Dcat\Admin\Widgets\StatItem;
+use Dcat\Admin\Widgets\IconicLink;
 use Dcat\Admin\Widgets\SimpleCard;
 use Dcat\Admin\Widgets\IconWithToolTip;
+use Illuminate\Contracts\Support\Renderable;
 
 class AccountDataCard extends SimpleCard
 {
 
     public function __construct()
     {
-        $start = (new StatItem(DcatIcon::HOME(true), 'Start', '10 days ago', '02.02.2023'))->render();
-        $end = (new StatItem(DcatIcon::HOME(true), 'End', 'Never', 'N/A'))->render();
-        $initBalance = (new StatItem(DcatIcon::HOME(true), 'Initail Balance', 'Deposited', '$200.000'))->render();
-        $platform = (new StatItem(DcatIcon::HOME(true), 'Platform', 'Trading platform', 'Metatrader4'))->render();
-        $broker = (new StatItem(DcatIcon::HOME(true), 'Broker', 'Trading Broker', 'Eightcap-Demo'))->render();
-        $share = (new StatItem(DcatIcon::HOME(true), 'Share', 'Lorem ipsum', '<a href="#">Link</a>'))->render();
+        $widgets = collect();
 
-        parent::__construct('Account Data', $start.$end.$initBalance.$platform.$broker.$share);
+        $widgets->push(new StatItem(DcatIcon::HOME(true), 'Start', '11 days ago', '02.02.2023'));
+        $widgets->push(new StatItem(DcatIcon::HOME(true), 'End', 'Never', 'N/A'));
+        $widgets->push(new StatItem(DcatIcon::HOME(true), 'Initail Balance', 'Deposited', '$200.000'));
+        $widgets->push(new StatItem(DcatIcon::HOME(true), 'Platform', 'Trading platform', 'Metatrader4'));
+        $widgets->push(new StatItem(DcatIcon::HOME(true), 'Broker', 'Trading Broker', 'Eightcap-Demo'));
+        $widgets->push(new StatItem(
+            DcatIcon::HOME(true),
+            'Share',
+            'Lorem ipsum',
+            (new IconicLink(DcatIcon::SHARE_ALT(true), 'Link', '#'))->render()
+        ));
+
+        parent::__construct('Account Data', $widgets->map(fn (Renderable $widget) => $widget->render())->join(''));
 
         $this->fullHeight();
         $this->tool(new IconWithToolTip(DcatIcon::HELP(), 'Account Data'));
