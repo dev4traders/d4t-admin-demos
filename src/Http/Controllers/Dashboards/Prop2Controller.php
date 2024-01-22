8742400ea9b4d719cca310f31a51c0b517baf3fc
mikha-dev <mikha.dev@gmail.com>
2024-01-22 16:50:53 +0300

    fix

--- src/Http/Controllers/Dashboards/Prop2Controller.php
+++ src/Http/Controllers/Dashboards/Prop2Controller.php
@@ -54,8 +54,8 @@ class Prop2Controller extends Controller
                 $widget->add('You are here', 'Step1 - Challenge Account', true);
                 $widget->add('Next Phase', 'Step2 - Funded Account');
                 $widget->add('Funded', 'Last stage');
-                $widget->stepActiveClass(StyleClassType::PRIMARY);
-                $widget->stepInactiveClass(StyleClassType::SECONDARY);
+                // $widget->stepActiveClass(StyleClassType::PRIMARY);
+                // $widget->stepInactiveClass(StyleClassType::SECONDARY);
 
                 $row->column(['sm' => 12], $widget);
             })
